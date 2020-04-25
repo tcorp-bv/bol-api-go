8287a655ba6ba01ddf092859f302977dce39f0ba
toonsevrin <twan123@live.be>
2020-04-25 14:26:14 +0200

    Fixed middleware

--- api.go
+++ api.go
@@ -2,6 +2,7 @@ package bolapi
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-openapi/runtime"
 	httptransport "github.com/go-openapi/runtime/client"
 	"github.com/go-openapi/strfmt"
@@ -17,6 +18,10 @@ const (
 	startBackoff = 1 * time.Second
 	maxBackoff   = 20 * time.Second
 	maxTries     = 50
+)
+
+var (
+	doNotRetryCodes = map[int]bool{200: true, 400: true}
 )
 
 // The core interface to retrieve the bol.com v3 client
@@ -37,21 +42,26 @@ type middleware struct {
 	maxTries  uint
 	backoff   backoff.Backoff
 	transport http.RoundTripper
+	verbose   bool
 }
 
 // Middleware to add retrying an
 func (m middleware) RoundTrip(req *http.Request) (*http.Response, error) {
 	var tries uint = 0
-	var err error
+	var res *http.Response
 	for tries <= m.maxTries {
 		res, err := m.transport.RoundTrip(req)
-		if err == nil {
-			return res, nil
+		if err != nil || res == nil || doNotRetryCodes[res.StatusCode] == true {
+			return res, err
 		}
-		time.Sleep(m.backoff.Get(tries))
+		t := m.backoff.Get(tries)
+		if m.verbose {
+			fmt.Printf("Sleeping %v with code %v\n.", t, res.StatusCode)
+		}
+		time.Sleep(t)
 		tries++
 	}
-	return nil, err
+	return res, nil
 }
 
 // Create a new API client with the host (default is "api.bol.com")
@@ -79,16 +89,17 @@ func newBolAPI(provider auth.CredentialProvider, host string) (*bolAPI, error) {
 		return nil, err
 	}
 	// Set up he transport to use the client with oauth
-	transport := httptransport.NewWithClient(host, "", nil, bolAuth.Client(context.Background()))
+	client := bolAuth.Client(context.Background())
+	client.Transport = middleware{
+		maxTries:  maxTries,
+		backoff:   backoff.NewExponentialBackoff(startBackoff, maxBackoff),
+		transport: client.Transport,
+	}
+	transport := httptransport.NewWithClient(host, "", nil, client)
 	// Map the consumers and producers to json
 	transport.Consumers["application/vnd.retailer.v3+json"] = runtime.JSONConsumer()
 	transport.Consumers["application/problem+json"] = runtime.JSONConsumer()
 	transport.Producers["application/vnd.retailer.v3+json"] = runtime.JSONProducer()
-	transport.Transport = middleware{
-		maxTries:  maxTries,
-		backoff:   backoff.NewExponentialBackoff(startBackoff, maxBackoff),
-		transport: transport.Transport,
-	}
 	return &bolAPI{Transport: transport}, nil
 }
 
